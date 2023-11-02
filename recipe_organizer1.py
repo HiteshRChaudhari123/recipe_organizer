c939af351f4b26060814cd464e3a51245800f376
HiteshRChaudhari123 <149664267+HiteshRChaudhari123@users.noreply.github.com>
2023-11-02 14:42:41 +0530

    Recipe_organizer1.py

--- recipe_organizer1.py
+++ recipe_organizer1.py
@@ -1,11 +1,18 @@
 import os
 
-if os.path.exists("/mnt/data/recipes.txt"):
+def load_recipes():
+    if not os.path.exists("/mnt/data/recipes.txt"):
+        os.makedirs("/mnt/data", exist_ok=True)  # Ensure the directory exists
+        with open("/mnt/data/recipes.txt", "w") as file:  # Create the file if it doesn't exist
+            file.write("{}")
+        print("No saved recipes found. Created a new recipe book.")
+        return {}
+    
     with open("/mnt/data/recipes.txt", "r") as file:
         content = file.read()
-        recipes = eval(content) if content else {}
-else:
-    recipes = {}
+        return eval(content) if content else {}
+
+recipes = load_recipes()
 
 def add_recipe():
     name = input("Enter the recipe name: ")
@@ -13,18 +20,27 @@ def add_recipe():
     instructions = input("Enter the instructions: ")
     recipes[name] = {'Ingredients': ingredients, 'Instructions': instructions}
     print(f"Recipe for {name} added successfully!")
-    with open("/mnt/data/recipes.txt", "w") as file:
-        file.write(str(recipes))
+    try:
+        with open("/mnt/data/recipes.txt", "w") as file:
+            file.write(str(recipes))
+    except Exception as e:
+        print(f"Error saving recipe: {e}")
 
 def view_recipe():
     print("\nAvailable Recipes:")
     for idx, name in enumerate(recipes.keys(), start=1):
         print(f"{idx}. {name}")
-    recipe_num = int(input("\nEnter the number of the recipe you want to view: "))
-    name = list(recipes.keys())[recipe_num - 1]
-    print(f"\nRecipe: {name}")
-    print(f"Ingredients: {', '.join(recipes[name]['Ingredients'])}")
-    print(f"Instructions: {recipes[name]['Instructions']}")
+    try:
+        recipe_num = int(input("\nEnter the number of the recipe you want to view: "))
+        if 1 <= recipe_num <= len(recipes):
+            name = list(recipes.keys())[recipe_num - 1]
+            print(f"\nRecipe: {name}")
+            print(f"Ingredients: {', '.join(recipes[name]['Ingredients'])}")
+            print(f"Instructions: {recipes[name]['Instructions']}")
+        else:
+            print("Invalid recipe number.")
+    except ValueError:
+        print("Please enter a valid number.")
 
 def search_recipe():
     keyword = input("Enter a keyword to search for a recipe: ")
